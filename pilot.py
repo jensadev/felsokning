65278231d0300fd507aaf8c6ca83c35a530bb11d
Jens Andreasson <jensandreasson77@gmail.com>
2023-10-16 16:24:27 +0200

    ✨pilot miniräknare

--- pilot.py
+++ pilot.py
@@ -1,11 +1,13 @@
 print('Extra lyxig miniräknare')
 while True:
     num1 = input('Skriv in tal 1: ')
-    num2 = input('Skriv in tal 2: ')
+    num1 = input('Skriv in tal 2: ')
 
-    menu = input('Tryck A för att addera och M för multiplikation')
+    menu = input('Tryck [A]ddition, [D]ivision, [M]ultiplikation, [E]xit')
 
     if menu == 'A':
         print(num1 + num2)
+    elif menu == 'D':
+        print(num1 % num2)
     else:
         print(int(num1) * int(num2))
