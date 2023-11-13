31662bcd19e60d6684ac279bf432a69a13c5459f
Jens Andreasson <jens.andreasson@ntig.se>
2023-11-13 16:28:36 +0100

    pilot version 2

--- pilot.py
+++ pilot.py
@@ -1,13 +1,13 @@
 print('Extra lyxig miniräknare')
 while True:
     num1 = input('Skriv in tal 1: ')
-    num1 = input('Skriv in tal 2: ')
+    num2 = input('Skriv in tal 2: ')
 
     menu = input('Tryck [A]ddition, [D]ivision, [M]ultiplikation, [E]xit')
 
     if menu == 'A':
         print(num1 + num2)
     elif menu == 'D':
-        print(num1 % num2)
+        print(num2 % num2)
     else:
-        print(int(num1) * int(num2))
+        print(int(num1) * int("num2"))
