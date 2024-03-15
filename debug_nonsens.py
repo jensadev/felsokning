90b7c44a93b2cd43746d1cc74774994d086c24bd
Jens Andreasson <jens.andreasson@ntig.se>
2024-03-15 13:05:18 +0100

    nonsens

---
+++ debug_nonsens.py
@@ -0,0 +1,18 @@
+# Påstående: eleverna försöker läsa funktion ur texten utan att förstå kod.
+# Det både hindrar dem och hjälper dem, det är en d¨ålig situation.
+print('Enon enonkoklolarore kokononvoverortoterorarore \nPoprorogogroramommometot soskokalollol erorsosätottota alollola sosifoffofrororor soskokrorivovnona momedod totexoxtot totilollol sosifoffofrororor.')
+dict = {"etottot":1,
+        "fofemom":5,
+        "totvovå":2,
+        "totrore":3,
+        "sosexox":6,
+        "fofyrora":4,
+        "totio":10}
+totexoxtot = "q"
+while ("q" in totexoxtot):
+    totexoxtot = input("Momatota inon enon momenoninongog momedod sosmomå bobokoksostotävoveror (q).")
+    for erorsosätottot in dict:
+        if erorsosätottot in totexoxtot:
+            totexoxtot.replace(erorsosätottot,dict[erorsosätottot])
+    print("Dodenon kokononvoverortoteroradode totexoxtotenon boblolevov sosomom foföloljojeror:\n")
+    print(totexoxtot)
